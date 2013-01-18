52e86070c717f8ce6e983d91675c812742404fe9
David A. Solin <david@joval.org>
2013-01-18 15:12:36 -0600

    Change name of local provider JAR file.

--- common.mk
+++ common.mk
@@ -50,5 +50,5 @@ FACADE=$(COMPONENTS)/facade
 FACADE_LIB=$(FACADE)/jSAF.jar
 FACADE_DEPS=$(subst $(SPACE),$(CLN),$(filter %.jar %.zip, $(wildcard $(FACADE)/$(LIBDIR)/*)))
 PROVIDER=$(COMPONENTS)/provider
-PROVIDER_LIB=$(PROVIDER)/jSAF-LocalProvider.jar
+PROVIDER_LIB=$(PROVIDER)/jSAF-Provider.jar
 PROVIDER_DEPS=$(subst $(SPACE),$(CLN),$(filter %.jar %.zip, $(wildcard $(PROVIDER)/$(LIBDIR)/*)))

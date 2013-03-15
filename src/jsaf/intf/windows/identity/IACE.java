831eb429bf4f7548612d71b983b83b640c058744
David A. Solin <david@joval.org>
2013-03-14 21:23:28 -0500

    Add Windows IACE constants pertaining to service rights.

--- src/jsaf/intf/windows/identity/IACE.java
+++ src/jsaf/intf/windows/identity/IACE.java
@@ -9,6 +9,7 @@ package jsaf.intf.windows.identity;
  * @see <a href="http://msdn.microsoft.com/en-us/library/windows/desktop/aa374896%28v=vs.85%29.aspx">Access Mask Format (Windows)</a>
  * @see <a href="http://msdn.microsoft.com/en-us/library/windows/desktop/aa374892%28v=vs.85%29.aspx">ACCESS_MASK (Windows)</a>
  * @see <a href="http://msdn.microsoft.com/en-us/library/windows/desktop/aa364399%28v=vs.85%29.aspx">File Security and Access Rights (Windows)</a>
+ * @see <a href="http://msdn.microsoft.com/en-us/library/windows/desktop/ms685981%28v=vs.85%29.aspx">Service Security and Access Rights (Windows)</a>
  *
  * @author David A. Solin
  * @version %I% %G%
@@ -69,6 +70,27 @@ public interface IACE {
     int FILE_GENERIC_WRITE = FILE_APPEND_DATA | FILE_WRITE_ATTRIBUTES | FILE_WRITE_DATA | FILE_WRITE_EA | STANDARD_RIGHTS_WRITE | SYNCHRONIZE;
     int FILE_GENERIC_EXECUTE = FILE_EXECUTE | FILE_READ_ATTRIBUTES | STANDARD_RIGHTS_EXECUTE | SYNCHRONIZE;
     int FILE_GENERIC_ALL = FILE_GENERIC_READ | FILE_GENERIC_WRITE | FILE_GENERIC_EXECUTE;
+
+    //
+    // Since 1.0.1
+    //
+    int SC_MANAGER_ALL_ACCESS		= 0xF003F;
+    int SC_MANAGER_CREATE_SERVICE	= 0x002;
+    int SC_MANAGER_CONNECT		= 0x0001;
+    int SC_MANAGER_ENUMERATE_SERVICE	= 0x0004;
+    int SC_MANAGER_LOCK			= 0x0008;
+    int SC_MANAGER_MODIFY_BOOT_CONFIG	= 0x0020;
+    int SC_MANAGER_QUERY_LOCK_STATUS	= 0x0010;
+    int SERVICE_ALL_ACCESS		= 0xF01FF;
+    int SERVICE_CHANGE_CONFIG		= 0x0002;
+    int SERVICE_ENUMERATE_DEPENDENTS	= 0x0008;
+    int SERVICE_INTERROGATE		= 0x0080;
+    int SERVICE_PAUSE_CONTINUE		= 0x0040;
+    int SERVICE_QUERY_CONFIG		= 0x0001;
+    int SERVICE_QUERY_STATUS		= 0x0004;
+    int SERVICE_START			= 0x0010;
+    int SERVICE_STOP			= 0x0020;
+    int SERVICE_USER_DEFINED_CONTROL	= 0x0100;
 
     /**
      * Get the entry access flags.

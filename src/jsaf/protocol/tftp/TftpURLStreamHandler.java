4c76bf0492a89f98b4c6f65b90fbf73087fc7f81
David A. Solin <david@joval.org>
2014-03-21 17:17:14 -0500

    Updated copyright headers on new files.

--- src/jsaf/protocol/tftp/TftpURLStreamHandler.java
+++ src/jsaf/protocol/tftp/TftpURLStreamHandler.java
@@ -1,4 +1,5 @@
 // Copyright (C) 2011 jOVAL.org.  All rights reserved.
+// This software is licensed under the LGPL 3.0 license available at http://www.gnu.org/licenses/lgpl.txt
 
 package jsaf.protocol.tftp;
 

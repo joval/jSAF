2cbe9b320fb2d2d03f69663a8f45ba78a8f08101
David A. Solin <david@joval.org>
2014-04-10 15:38:39 -0500

    Doc modification.

--- src/jsaf/intf/service/IDatabase.java
+++ src/jsaf/intf/service/IDatabase.java
@@ -20,16 +20,25 @@ public interface IDatabase {
      * An enumeration of relational ratabase engines.
      */
     enum Engine {
-	DB2(50000),
-	MSSQL(1433),
-	MYSQL(3306),
-	ORACLE(1521),
-	POSTGRESQL(5432);
+	DB2("jdbc:db2://", 50000),
+	MSSQL("jdbc:sqlserver://", 1433),
+	MYSQL("jdbc:mysql://", 3306),
+	ORACLE("jdbc:oracle:thin:@//", 1521),
+	POSTGRESQL("jdbc:postgresql://", 5432);
 
 	private int port;
+	private String prefix;
 
-	private Engine(int port) {
+	private Engine(String prefix, int port) {
+	    this.prefix = prefix;
 	    this.port = port;
+	}
+
+	/**
+	 * Return the "prefix" portion of the JDBC connection URL for this engine.
+ 	 */
+	public String getPrefix() {
+	    return prefix;
 	}
 
 	public int getDefaultPort() {
@@ -40,7 +49,8 @@ public interface IDatabase {
     /**
      * Get a JDBC connection to the specified database.
      *
-     * @param name Corresponds to the desired Oracle Service Name or MS SQL database instance name.
+     * @param name Corresponds to the desired Oracle Service Name or DB2/MSSQL/MYSQL/PostgreSQL database instance name. To
+     *             specify a particular MSSQL Server instance, use the convention INSTANCENAME\\DATABASENAME.
      * @param cred The database login credential.
      *
      * @since 1.0

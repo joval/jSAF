1a81b740d3c8f50aff19da5f63e6b1dd1eabdb67
Adam Biggs <adam.biggs@arcticwolf.com>
2023-06-30 11:26:36 -0300

    Changing Enum case to match existing code convention

--- src/jsaf/util/Regex.java
+++ src/jsaf/util/Regex.java
@@ -32,7 +32,7 @@ public class Regex {
   // Characters that have special meanings within a regex for which escaping is required to match the literal character.
   private static final String[] SPECIAL_CHARACTERS = { ESCAPE, "^", ".", "$", "|", "(", ")", "[", "]", OBRACE, CBRACE, "*", "+", "?" };
 
-  public static enum REGEX_CONCEPT {
+  public static enum RegexConcept {
     // . - Match any character (except newline)
     DOT("."),
     // () - Grouping
@@ -92,7 +92,7 @@ public class Regex {
 
     private String s;
 
-    REGEX_CONCEPT(String s) {
+    RegexConcept(String s) {
       this.s = s;
     }
 
@@ -106,8 +106,8 @@ public class Regex {
     /**
      * Given a String value, obtain a corresponding REGEX_CONCEPT.
      */
-    public static REGEX_CONCEPT typeOf(String s) {
-      for (REGEX_CONCEPT t : values()) {
+    public static RegexConcept typeOf(String s) {
+      for (RegexConcept t : values()) {
         if (t.s.equals(s)) {
           return t;
         }
@@ -116,48 +116,48 @@ public class Regex {
     }
   }
 
-  static final Map<REGEX_CONCEPT, Pattern> REGEX_REGEX = Collections.unmodifiableMap(new HashMap<REGEX_CONCEPT, Pattern>() {
+  static final Map<RegexConcept, Pattern> REGEX_REGEX = Collections.unmodifiableMap(new HashMap<RegexConcept, Pattern>() {
     {
       // . - Match any character (except newline)
-      put(REGEX_CONCEPT.DOT, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\..*"));
+      put(RegexConcept.DOT, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\..*"));
       // () - Grouping
-      put(REGEX_CONCEPT.GROUPING, Pattern.compile("(.*?(?<!\\\\)(?:\\\\{2})*)\\(.*?(?<!\\\\)(?:\\\\{2})*\\).*"));
+      put(RegexConcept.GROUPING, Pattern.compile("(.*?(?<!\\\\)(?:\\\\{2})*)\\(.*?(?<!\\\\)(?:\\\\{2})*\\).*"));
       // [] - Character class (also matches [^])
-      put(REGEX_CONCEPT.CHARACTER_CLASS, Pattern.compile("(.*?(?<!\\\\)(?:\\\\{2})*)\\[\\^?.*?(?<!\\\\)(?:\\\\{2})*\\].*"));
+      put(RegexConcept.CHARACTER_CLASS, Pattern.compile("(.*?(?<!\\\\)(?:\\\\{2})*)\\[\\^?.*?(?<!\\\\)(?:\\\\{2})*\\].*"));
       // | - Alternation
-      put(REGEX_CONCEPT.ALTERNATION, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\|(?![^\\[\\]]*\\]).*"));
+      put(RegexConcept.ALTERNATION, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\|(?![^\\[\\]]*\\]).*"));
       // + - Match 1 or more times
-      put(REGEX_CONCEPT.PLUS, Pattern.compile("((?!.*\\[.*\\\\?\\+.*\\].*$).*[^\\\\](?:\\\\{2})*)\\+.*"));
+      put(RegexConcept.PLUS, Pattern.compile("((?!.*\\[.*\\\\?\\+.*\\].*$).*[^\\\\](?:\\\\{2})*)\\+.*"));
       // * - Match 0 or more times
-      put(REGEX_CONCEPT.STAR, Pattern.compile("((?!.*\\[.*\\\\?\\*.*\\].*$).*[^\\\\](?:\\\\{2})*)\\*.*"));
+      put(RegexConcept.STAR, Pattern.compile("((?!.*\\[.*\\\\?\\*.*\\].*$).*[^\\\\](?:\\\\{2})*)\\*.*"));
       // ? - Match 1 or 0 times
-      put(REGEX_CONCEPT.QUESTION, Pattern.compile("((?!.*\\[.*\\\\?\\?.*\\].*$).*[^\\\\](?:\\\\{2})*)\\?.*"));
+      put(RegexConcept.QUESTION, Pattern.compile("((?!.*\\[.*\\\\?\\?.*\\].*$).*[^\\\\](?:\\\\{2})*)\\?.*"));
       // ^ - Match the beginning of the line (does not match [^])
-      put(REGEX_CONCEPT.LINE_START, Pattern.compile("((?:|[^\\\\](?:\\\\{2})*\\^.*|(?!.*\\[.*?\\\\?\\^.*\\].*$).*[^\\\\])(?:\\\\{2})*)\\^.*"));
+      put(RegexConcept.LINE_START, Pattern.compile("((?:|[^\\\\](?:\\\\{2})*\\^.*|(?!.*\\[.*?\\\\?\\^.*\\].*$).*[^\\\\])(?:\\\\{2})*)\\^.*"));
       // $ - Match the end of the line (or before newline at the end)
-      put(REGEX_CONCEPT.LINE_END, Pattern.compile("((?:|[^\\\\](?:\\\\{2})*\\$.*|(?!.*\\[.*\\\\?\\$.*\\].*$).*[^\\\\])(?:\\\\{2})*)\\$.*"));
+      put(RegexConcept.LINE_END, Pattern.compile("((?:|[^\\\\](?:\\\\{2})*\\$.*|(?!.*\\[.*\\\\?\\$.*\\].*$).*[^\\\\])(?:\\\\{2})*)\\$.*"));
       // \w - Match a "word" character (alphanumeric plus "_")
-      put(REGEX_CONCEPT.WORD, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\\\w.*"));
+      put(RegexConcept.WORD, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\\\w.*"));
       // \W - Match a non-word character
-      put(REGEX_CONCEPT.NONWORD, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\\\W.*"));
+      put(RegexConcept.NONWORD, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\\\W.*"));
       // \s - Match a whitespace character
-      put(REGEX_CONCEPT.WHITESPACE, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\\\s.*"));
+      put(RegexConcept.WHITESPACE, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\\\s.*"));
       // \S - Match a non-whitespace character
-      put(REGEX_CONCEPT.NONWHITESPACE, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\\\S.*"));
+      put(RegexConcept.NONWHITESPACE, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\\\S.*"));
       // \d - Match a digit character
-      put(REGEX_CONCEPT.DIGIT, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\\\d.*"));
+      put(RegexConcept.DIGIT, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\\\d.*"));
       // \D - Match a non-digit character
-      put(REGEX_CONCEPT.NONDIGIT, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\\\D.*"));
+      put(RegexConcept.NONDIGIT, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\\\D.*"));
       // \b - Match a word boundary
-      put(REGEX_CONCEPT.WORD_BOUNDARY, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\\\b.*"));
+      put(RegexConcept.WORD_BOUNDARY, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\\\b.*"));
       // \B - Match a non-(word boundary)
-      put(REGEX_CONCEPT.NONWORD_BOUNDARY, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\\\B.*"));
+      put(RegexConcept.NONWORD_BOUNDARY, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\\\B.*"));
       // {n} - Match exactly n times
-      put(REGEX_CONCEPT.MATCH_EXACTLY, Pattern.compile("(.*?[^\\\\](?:\\{2})*)\\{[0-9]+\\}(?![^\\[\\]]*\\]).*"));
+      put(RegexConcept.MATCH_EXACTLY, Pattern.compile("(.*?[^\\\\](?:\\{2})*)\\{[0-9]+\\}(?![^\\[\\]]*\\]).*"));
       // {n,} - Match at least n times
-      put(REGEX_CONCEPT.MATCH_ATLEAST, Pattern.compile("(.*?[^\\\\](?:\\{2})*)\\{[0-9]+,\\}(?![^\\[\\]]*\\]).*"));
+      put(RegexConcept.MATCH_ATLEAST, Pattern.compile("(.*?[^\\\\](?:\\{2})*)\\{[0-9]+,\\}(?![^\\[\\]]*\\]).*"));
       // {n,m} - Match at least n but not more than m times
-      put(REGEX_CONCEPT.MATCH_BETWEEN, Pattern.compile("(.*?[^\\\\](?:\\{2})*)\\{[0-9]+,[0-9]+\\}(?![^\\[\\]]*\\]).*"));
+      put(RegexConcept.MATCH_BETWEEN, Pattern.compile("(.*?[^\\\\](?:\\{2})*)\\{[0-9]+,[0-9]+\\}(?![^\\[\\]]*\\]).*"));
       // \p{Lower} - A lower-case alphabetic character: [a-z]
       // \p{Upper}	An upper-case alphabetic character:[A-Z]
       // \p{ASCII}	All ASCII:[\x00-\x7F]
@@ -171,7 +171,7 @@ public class Regex {
       // \p{Cntrl}	A control character: [\x00-\x1F\x7F]
       // \p{XDigit}	A hexadecimal digit: [0-9a-fA-F]
       // \p{Space}	A whitespace character: [ \t\n\x0B\f\r]
-      put(REGEX_CONCEPT.POSIX, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\\\p\\{(Lower|Upper|ASCII|Alpha|Digit|Alnum|Punct|Graph|Print|Blank|Cntrl|XDigit|Space)\\}.*"));
+      put(RegexConcept.POSIX, Pattern.compile("(.*?(?<!\\\\)(?:\\\\\\\\)*)\\\\p\\{(Lower|Upper|ASCII|Alpha|Digit|Alnum|Punct|Graph|Print|Blank|Cntrl|XDigit|Space)\\}.*"));
     }
   });
 
@@ -208,21 +208,21 @@ public class Regex {
    * @param excludedConcepts Regex patterns to exclude.
    * @return The longest prefix in the pattern string for which there is no regex expression characters.
    */
-  public static String findLongestNonRegexPrefix(Pattern pattern, REGEX_CONCEPT... excludedConcepts) {
+  public static String findLongestNonRegexPrefix(Pattern pattern, RegexConcept... excludedConcepts) {
     String s = pattern.pattern();
     if (s.equals("")) {
       return s;
     }
-    List<REGEX_CONCEPT> concepts = new ArrayList<REGEX_CONCEPT>(REGEX_REGEX.keySet());
-    for (REGEX_CONCEPT regexConcept : excludedConcepts) {
+    List<RegexConcept> concepts = new ArrayList<RegexConcept>(REGEX_REGEX.keySet());
+    for (RegexConcept regexConcept : excludedConcepts) {
       concepts.remove(regexConcept);
     }
     String subString = s;
-    for (REGEX_CONCEPT regexConcept : concepts) {
+    for (RegexConcept regexConcept : concepts) {
       Matcher matcher = REGEX_REGEX.get(regexConcept).matcher(s);
       if (matcher.matches()) {
         String temp = matcher.group(1);
-        if (regexConcept == REGEX_CONCEPT.STAR || regexConcept == REGEX_CONCEPT.PLUS || regexConcept == REGEX_CONCEPT.QUESTION) {
+        if (regexConcept == RegexConcept.STAR || regexConcept == RegexConcept.PLUS || regexConcept == RegexConcept.QUESTION) {
           if (temp.length() - 1 < subString.length()) {
             subString = temp.substring(0, temp.length() - 1);
           }
@@ -411,7 +411,7 @@ public class Regex {
    * Returns true if the specified regex expression contains the specified regular expression syntax.  This function take into account escaped regex characters.
    * Also, returns false if there is no regex pattern defined for the REGEX_CONCEPT.
    * */
-  public static boolean containsSpecificRegex(REGEX_CONCEPT regex, String pattern) throws PatternSyntaxException {
+  public static boolean containsSpecificRegex(RegexConcept regex, String pattern) throws PatternSyntaxException {
     Pattern p = REGEX_REGEX.get(regex);
     return p != null && p.matcher(pattern).matches();
   }
